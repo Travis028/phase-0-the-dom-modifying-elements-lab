e45ef6cd702aca380818b51629775ff21fa696e6
waynetravis <waynetavis123@gmail.com>
2025-06-15 22:34:59 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Remove the main element
+const main = document.getElementById("main");
+if (main) {
+    main.remove();
+}
+
+// Create new header
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "wayne is the champion";
+
+document.body.appendChild(newHeader);
